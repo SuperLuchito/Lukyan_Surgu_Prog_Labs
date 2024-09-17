72233ba22891940015fcb4d45aa2e69654fff88d
luchok503 <lukyannib@mail.ru>
2024-09-17 18:41:17 +0500

    Lab-5.cpp

---
+++ Lab-5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int k;
+
+    cin >> k;
+
+    int R[k];
+
+    int min_positive = 10000000;
+    int max_negative = -1000000;
+
+    for (int i = 0; i < k; i++) {
+        cin >> R[i];
+        if ((R[i] >= 0) and (R[i] < min_positive)) {
+            min_positive = R[i];
+        } else if ((R[i] < 0) and (R[i] > max_negative)) {
+            max_negative = R[i];
+        }
+    }
+
+    cout << "Минимальный элемент среди положительных: " << min_positive << endl << "Максимальный среди отрицательных: " << max_negative;
+
+    return 0;
+}
